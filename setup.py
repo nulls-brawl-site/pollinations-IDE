0ef9e7b9bd7f66e5424533cc53d6b28a3f2ee377
Nulls Utilities <nullsbrawlmods@gmail.com>
2026-02-01 20:09:04 +0500

    Bump version from 2.2.0 to 2.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="polly-ide",
-    version="2.2.0", # <-- BUMP VERSION
+    version="2.3.0", # <-- BUMP VERSION
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
